25f7e284c46bb9fc8ad1fdb75060b811788933e3
Amos Ng <amosjyng@gmail.com>
2014-03-27 16:22:38 -0400

    Simplified script

--- glasso.R
+++ glasso.R
@@ -1,14 +1,13 @@
-library(glasso)
+library(glasso) # keep an eye out for dpglasso
 
 # read data from commandline argument
-L1_penalty <- 1
 args <- commandArgs(trailingOnly = TRUE)
 data <- read.delim(args[1])
 s <- var(data)
 
 # run GLASSO algorithm
-gm <- glasso(s, L1_penalty)
+gm <- glasso(s, 50)
 
 # do something with results
 save(gm, file="glasso_model.Rda")
-print(gm$loglik)
+print(paste("Log-likelihoode of model is", toString(gm$loglik)))
